5974a8ba8a6668339ac4aa44fcfc477221bb3cd2
yeweimao <yeweimao@zhuanzhuan.com>
2022-09-18 00:08:37 +0800

    feat: vite预编译阶段

---
+++ viteuse/dist/bundle.js
@@ -0,0 +1,3 @@
+// src/main.js
+import { createApp } from "/Users/ywm/learnProject/zhufengPoject/engineered/viteuse/node_modules/vue/dist/vue.runtime.esm-bundler.js";
+console.log(createApp);
--- viteuse/vite.config.js
+++ viteuse/vite.config.js
@@ -1,10 +1,16 @@
 /*
  * @Author: yeweimao yeweimao@zhuanzhuan.com
- * @Date: 2022-09-16 00:22:30
+ * @Date: 2022-09-17 21:25:25
  * @LastEditors: yeweimao yeweimao@zhuanzhuan.com
- * @LastEditTime: 2022-09-17 16:32:32
- * @FilePath: /engineered/viteuse/src/main.js
+ * @LastEditTime: 2022-09-17 21:25:31
+ * @FilePath: /viteuse/vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { createApp } from "vue";
-console.log(createApp);
+const path = require("path");
+module.exports = {
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
+    },
+  },
+};
